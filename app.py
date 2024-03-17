6e249b6b793174aa4bef8d3663a8856cd160faec
NickMalberg <nick.malberg@gmail.com>
2024-03-17 22:04:41 +0100

    init

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
+
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
+app.config["UPLOAD_FOLDER"] = "static/files"
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+class UploadFileForm(FlaskForm):
+    file = FileField("File")
+    submit = SubmitField("Submit")
+
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        return "File has been uploaded"
+    return render_template("index.html", form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
